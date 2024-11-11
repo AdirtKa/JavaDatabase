ab8bdf90911e26ceb00baf07740d9ca25cae1d8d
AdirtKaLaptop <AdirtKa@yandex.ru>
2024-11-11 20:52:44 +1000

    make a toy code

--- src/main/java/com/adirtka/database/HelloController.java
+++ src/main/java/com/adirtka/database/HelloController.java
@@ -2,13 +2,27 @@ package com.adirtka.database;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import java.sql.*;
 
 public class HelloController {
+    String url = "jdbc:postgresql://localhost:5432/JavaTimetable";
+    String user = "postgres";
+    String password = "admin";
+
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)){
+            Statement test = connection.createStatement();
+            ResultSet answer = test.executeQuery("SELECT * FROM classes");
+            answer.next();
+            System.out.println(answer.getString("class_name"));
+        } catch (SQLException e){
+            System.out.println("Ошибка подключения: " + e.getMessage());
+        }
     }
 }
