8bc197e8a225b5155ff5401e5f73265dd87834ad
AdirtKa <AdirtKa@yandex.ru>
2024-11-11 22:24:41 +1000

    hide credentials in property file

--- src/main/java/com/adirtka/database/HelloController.java
+++ src/main/java/com/adirtka/database/HelloController.java
@@ -2,12 +2,17 @@ package com.adirtka.database;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.*;
+import java.util.Properties;
 
 public class HelloController {
-    String url = "jdbc:postgresql://localhost:5432/JavaTimetable";
-    String user = "postgres";
-    String password = "admin";
+
+    String url = "jdbc:postgresql://%s:5432/JavaTimetable";
+    String user = "adirtka";
 
     @FXML
     private Label welcomeText;
@@ -15,6 +20,20 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+
+        Properties properties = new Properties();
+
+        final String configFilePath = "src/main/resources/config.properties";
+        try (FileInputStream input = new FileInputStream(configFilePath)) {
+            properties.load(input);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IOException e){
+            System.out.println("Ошибка чтения конфигурации: " + e.getMessage());
+        }
+
+        String password = properties.getProperty("db.password");
+        url = String.format(url, properties.getProperty("db.address"));
 
         try (Connection connection = DriverManager.getConnection(url, user, password)){
             Statement test = connection.createStatement();
