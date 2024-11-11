f7c26eabf6ea5934ddf5a0c084e026c949c5d13e
AdirtKaLaptop <AdirtKa@yandex.ru>
2024-11-11 19:37:08 +1000

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.adirtka.database {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.adirtka.database to javafx.fxml;
+    exports com.adirtka.database;
+}
